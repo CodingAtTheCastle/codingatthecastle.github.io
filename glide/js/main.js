b089450a749cf8d16a87a0453c7812ae7938ab61
Joel Drake <joel@infoping.se>
2017-11-22 22:12:29 +0100

    hidden

--- glide/js/main.js
+++ glide/js/main.js
@@ -2,3 +2,11 @@ $("#glide_test").glide({
     type: "carousel",
     autoheight: true
 });
+
+
+/*
+Hidden ligger med från start på vissa objekt för att förhindra att man ser det innan glide har startats.
+Nu när glide är startad så kan hidden tas bort från dessa objekt.
+*/
+$('.glide__arrows').removeClass('hidden');
+$('.glide__slide').removeClass('hidden');
