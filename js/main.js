7dbd7ffba00cc7f01e4e12f46d8ea7d66bfb2edb
Joel Drake <joel@infoping.se>
2017-11-23 09:50:20 +0100

    Added a cool parallax effects to the header background

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 /* Insert cool JavaScript here */
-
 console.log('Castle Coding for the win 🦄');
+
+window.addEventListener('scroll', scrollStuff);
+
+function scrollStuff() {
+    /*
+    Adding cool parallax effects to the header background
+    Also adding a fadeout, because that it pretty sweet
+    */
+
+    const windowScrolltop = window.scrollY;
+    const header_background = document.querySelector('.header_background');
+    const transformValue = (windowScrolltop / 2);
+    const opacityValue = 1 - (windowScrolltop / 400);
+
+    header_background.style.transform = 'translateY('+transformValue+'px)';
+    header_background.style.opacity = opacityValue;
+}
