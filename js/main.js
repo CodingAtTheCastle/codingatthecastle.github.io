4dc3db00694e23ffac401a7b8d2f5f619ab2cfce
Joel Drake <joel@infoping.se>
2017-11-29 22:39:32 +0100

    fixed map

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ function scrollStuff() {
 
     const windowScrolltop = window.scrollY;
     const header_background = document.querySelector('.header_background');
+    const map_mapparallax = document.querySelector('.map_parallax');
     const transformValue = (windowScrolltop / 2);
     const opacityValue = 1 - (windowScrolltop / 400);
 
@@ -28,9 +29,113 @@ function scrollStuff() {
 }
 
 function initMap() {
-    // Create a map object and specify the DOM element for display.
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
+
+    var map_obj = document.getElementById('googlemap');
+    var map_style = [
+        {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+        {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+        {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+        {
+          featureType: 'administrative.locality',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'poi',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'geometry',
+          stylers: [{color: '#263c3f'}]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#6b9a76'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry',
+          stylers: [{color: '#38414e'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#212a37'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#9ca5b3'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry',
+          stylers: [{color: '#746855'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#1f2835'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#f3d19c'}]
+        },
+        {
+          featureType: 'transit',
+          elementType: 'geometry',
+          stylers: [{color: '#2f3948'}]
+        },
+        {
+          featureType: 'transit.station',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'geometry',
+          stylers: [{color: '#17263c'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#515c6d'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.stroke',
+          stylers: [{color: '#17263c'}]
+        }
+    ];
+
+    var theCastleLatLng = {lat: 59.3261685, lng: 18.0739393};
+
+    var map = new google.maps.Map(map_obj, {
+      center: theCastleLatLng,
+      zoom: 15,
+      fullscreenControl: false,
+      mapTypeControl: false,
+      streetViewControl: false,
+      styles: map_style,
     });
+
+    var markerImage = 'img/CastleCodingMarker.png';
+    var markerImage = {
+        url: 'img/CastleCodingMarker.png',
+        scaledSize: new google.maps.Size(100, 108)
+      };
+    var marker = new google.maps.Marker({
+        position: theCastleLatLng,
+        map: map,
+        animation: google.maps.Animation.DROP,
+        title: 'Castle Coding',
+        icon: markerImage,
+    });
+
+
 }
+
