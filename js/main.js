8be094a4e9ba6af572a4016c7a4e6b56bd1a0b1e
Joel Drake <joel@infoping.se>
2017-11-29 19:36:37 +0100

    testar en grej

--- js/main.js
+++ js/main.js
@@ -26,3 +26,11 @@ function scrollStuff() {
     }
 
 }
+
+function initMap() {
+    // Create a map object and specify the DOM element for display.
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+}
